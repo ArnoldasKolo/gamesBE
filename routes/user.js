f7df73e12d1b60f52810b200dd216e62f60f425c
Arnoldas <akolosevskis28@gmail.com>
2023-06-29 17:52:18 +0300

    First commit

---
+++ routes/user.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const authMiddleware = require("../middleware/auth");
+const { 
+  LOGIN,
+  SIGNUP,
+  GET_USER_BY_ID,
+  GET_ALL_USERS_POSTS,
+  UPDATE_USER,
+} = require("../controllers/user");
+
+router.post("/signUp", SIGNUP);
+router.post("/logIn", LOGIN);
+router.get("/user/:id", GET_USER_BY_ID);
+router.get("/AllUsersPosts",authMiddleware, GET_ALL_USERS_POSTS);
+router.put("/user/info/:id", authMiddleware,UPDATE_USER);
+
+module.exports = router;
